b40bb9696f73ae374bd2571b93794d30ae53b352
Enric Lluelles <enriclluelles@gmail.com>
2011-01-11 23:07:10 +0100

    Those should be then steps, learning cukes the hard way

--- features/step_definitions/sidebar_steps.rb
+++ features/step_definitions/sidebar_steps.rb
@@ -1,11 +1,11 @@
-When /^I should see the project "([^\"]*)"$/ do |project_name|
+Then /^I should see the project "([^\"]*)"$/ do |project_name|
   Then %(I should see "#{project_name}" within "#my_projects")
 end
 
-When /^I should see the task "([^\"]*)" in the sidebar$/ do |task_name|
+Then /^I should see the task "([^\"]*)" in the sidebar$/ do |task_name|
   Then %(I should see "#{task_name}" within "#my_tasks")
 end
 
-When /^I should see the organization "([^\"]*)" in the sidebar$/ do |organization_name|
+Then /^I should see the organization "([^\"]*)" in the sidebar$/ do |organization_name|
   Then %(I should see "#{organization_name}" within "#my_organizations")
 end
