f43ce2b35c9d2c0fbbd001ddc022b7395f497e10
Jordi Romero <jordi@jrom.net>
2011-06-30 10:47:28 +0200

    WIP api v2

--- config/routes.rb
+++ config/routes.rb
@@ -242,6 +242,10 @@ Teambox::Application.routes.draw do
       end
     end
 
+    namespace :api_v2, :path => 'api/2' do
+      resources :activities, :only => [:index, :show]
+    end
+
     namespace :api_v1, :path => 'api/1' do
       resources :projects, :except => [:new, :edit] do
         member do
